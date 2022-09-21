6013331d84844246e9199a83b456818bd22a932b
sultanshaik <sultanshaik.dev@gmail.com>
2022-09-21 11:29:13 -0500

    Remove redundant parseint statements

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import useDebounce from "../utils/useDebounce";
 
 import CloseIcon from '@mui/icons-material/Close';
 
 const Input = ({suggestions, getSuggestions, setSymbolValue})=>{
-    console.log(suggestions);
     const [searchTxt, setSearchTxt]  =useState('');
     const delayedCall =  useDebounce(()=>getSuggestions(searchTxt), 200)
 
@@ -14,26 +13,26 @@ const Input = ({suggestions, getSuggestions, setSymbolValue})=>{
 
     const isAlpha = (keyCode)=>{    
     //Small Alphabets
-    if(parseInt(keyCode)>=97 && parseInt(keyCode)<=122){return true;}
+    if(keyCode>=97 && keyCode<=122){return true;}
     //Caps Alphabets
-    if(parseInt(keyCode)>=65 && parseInt(keyCode)<=90){return true;}
+    if(keyCode>=65 &&keyCode<=90){return true;}
     return false; 
     }
 
     const isSpecialInput = (keyCode) =>{
-        if(parseInt(keyCode)==32 ||
-        parseInt(keyCode)==13 ||
-            parseInt(keyCode)==33 ||
-                keyCode==8/*BCKSPC*/ || 
-                keyCode==37/*LFT ARROW*/ ||
-                keyCode==39/*RGT ARROW*/ )
+        if(keyCode==32 ||
+            keyCode==13 ||
+            keyCode==33 ||
+            keyCode==8/*BCKSPC*/ || 
+            keyCode==37/*LFT ARROW*/ ||
+            keyCode==39/*RGT ARROW*/ )
                 {
                     return true;
                 }
     }
 
 
-    const handleKeyEvents = (event)=>{
+    const handleKeyEvents = useCallback((event)=>{
         const keyCode = parseInt(event.which ? event.which : event.code);
         if(!isAlpha(keyCode) && !isSpecialInput(keyCode)){
             return;
@@ -45,10 +44,10 @@ const Input = ({suggestions, getSuggestions, setSymbolValue})=>{
         else if(keyCode===8){
             setSearchTxt(currentValue=>currentValue.slice(0,currentValue.length-1))
         }
-    }
-    const handleCloseEvent = () =>{
+    })
+    const handleCloseEvent = useCallback(() =>{
             setSearchTxt('')
-    }
+    });
     
     return <div className="auto-complete">
         <div className="input-field">
